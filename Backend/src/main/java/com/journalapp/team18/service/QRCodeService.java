48ae95cba154d7dd27a61fa54dd8d16e5be102ae
Hardeep Bainiwal <hardeepbainiwal26wp@gmail.com>
2026-05-05 09:27:04 +0000

    Add service classes for event management including Analytics, CheckIn, Email, Event, Payment, QRCode, Registration, and User services

---
+++ Backend/src/main/java/com/journalapp/team18/service/QRCodeService.java
@@ -0,0 +1,41 @@
+package com.eventmanagement.service;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.stereotype.Service;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
+
+@Service
+public class QRCodeService {
+
+    public String generateUniqueToken() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public String generateQRCodeBase64(String data, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+
+        return Base64.getEncoder().encodeToString(pngData);
+    }
+
+    public byte[] generateQRCodeImage(String data, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(data, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+
+        return pngOutputStream.toByteArray();
+    }
+}
